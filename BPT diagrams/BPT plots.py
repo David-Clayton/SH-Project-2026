69c42ae51a4a7a71e653b505eae02968d54432b9
David-Clayton <s2413476@ed.ac.uk>
2026-03-07 21:16:11 +0000

    Updated BPT diagrams with errorbars & labels of ionisation sources

--- BPT diagrams/BPT plots.py
+++ BPT diagrams/BPT plots.py
@@ -45,31 +45,40 @@ def extract_and_plot():
     h_b_err_down = pd.read_excel(filepath, usecols = "AP", skiprows = 0).to_numpy().flatten()
     h_a_err_down = pd.read_excel(filepath, usecols = "BN", skiprows = 0).to_numpy().flatten()
 
+    #average errors
+
+    o_iii_5007_err = (o_iii_5007_err_down + o_iii_5007_err_up) / 2
+    n_ii_6584_err = (n_ii_6584_err_down + n_ii_6584_err_up) / 2
+    s_ii_6717_err = (s_ii_6717_err_down + s_ii_6717_err_up) / 2
+    s_ii_6731_err = (s_ii_6731_err_down + s_ii_6731_err_up) / 2
+    o_i_6300_err = (o_i_6300_err_down + o_i_6300_err_up) / 2
+    h_b_err = (h_b_err_down + h_b_err_up) / 2
+    h_a_err = (h_a_err_down + h_a_err_up) / 2
+    
 
     #add sulphur doublet line intensities together to get combined intensity
 
     s_ii_6717_31 = s_ii_6717 + s_ii_6731
-    s_ii_6717_31_err_up = np.sqrt(s_ii_6717_err_up **2 + s_ii_6731_err_up ** 2)
-    s_ii_6717_31_err_down = np.sqrt(s_ii_6717_err_down **2 + s_ii_6731_err_down ** 2)
+    s_ii_6717_31_err = np.sqrt(s_ii_6731_err**2 + s_ii_6717_err**2)
     
-
-    #Get axis data 
-    x_tl = np.log10(n_ii_6584/h_a)
-    x_tl_err_up = x_tl * np.sqrt((n_ii_6584_err_up ** 2 / n_ii_6584 **2) + (h_a_err_up **2 / h_a **2 ))
-    x_tl_err_down = x_tl * np.sqrt((n_ii_6584_err_down ** 2 / n_ii_6584 **2) + (h_a_err_down **2 / h_a **2 ))
+    #Get axis data for the BPT diagrams
+    #Top left
+    nii_ha = np.log10(n_ii_6584/h_a)
+    nii_ha_err = np.sqrt((n_ii_6584_err/n_ii_6584) **2 + (h_a_err/h_a)**2) / np.log(10)
     
-    x_tr = np.log10(s_ii_6717_31/h_a)
-    x_tr_err_up = x_tr * np.sqrt((s_ii_6717_31_err_up ** 2 / s_ii_6717_31 **2) + (h_a_err_up **2 / h_a **2 ))
-    x_tr_err_down = x_tr * np.sqrt((s_ii_6717_31_err_down ** 2 / s_ii_6717_31 **2) + (h_a_err_down **2 / h_a **2 ))
+    #Top right
+    sii_ha = np.log10(s_ii_6717_31/h_a)
+    sii_ha_err = np.sqrt((s_ii_6717_31_err/s_ii_6717_31) **2 + (h_a_err/h_a)**2) / np.log(10)
 
-    x_b = np.log10(o_i_6300/h_a)
-    x_b_err_up = x_b * np.sqrt((o_i_6300_err_up ** 2 / o_i_6300 **2) + (h_a_err_up **2 / h_a **2 ))
-    x_b_err_down = x_b * np.sqrt((o_i_6300_err_down ** 2 / o_i_6300 **2) + (h_a_err_down **2 / h_a **2 ))
+    #Bottom
+    oi_ha = np.log10(o_i_6300/h_a)
+    oi_ha_err = np.sqrt((o_i_6300_err/o_i_6300) **2 + (h_a_err/h_a)**2) / np.log(10)
     
-    y = np.log10(o_iii_5007/h_b)
-    y_err_up = y * np.sqrt((o_iii_5007_err_up ** 2 / o_iii_5007 **2) + (h_b_err_up **2 / h_b **2 ))
-    y_err_down = y * np.sqrt((o_iii_5007_err_down ** 2 / o_iii_5007 **2) + (h_b_err_down **2 / h_b **2 ))
+    #y-axis
+    oiii_hb = np.log10(o_iii_5007/h_b)
+    oiii_hb_err = np.sqrt((o_iii_5007_err/o_iii_5007) **2 + (h_b_err/h_b)**2) / np.log(10)
 
+    
     x_kauff = np.linspace(-3, 0, 1000)
     y_kauff = n2kauffmann(x_kauff)
 
@@ -79,13 +88,15 @@ def extract_and_plot():
     y_kewleyo1 = o1kewley(x_kewley)
 
     #plot
-    
-    plt.scatter(x_tl, y, s = 4, c = "r")
+    #NII BPT diagram
     plt.plot(x_kauff, y_kauff, linestyle = "--", c = "k", label = "Kauffman 2003")
     plt.plot(x_kewley, y_kewleyn2, linestyle = "-", c = "k", label = "Kewley 2001")
-    #plt.errorbar(x_tl, y, yerr = [y_err_down / (np.log(10) * y), y_err_up  / (np.log(10) * y)], xerr = [x_tl_err_down / (np.log(10)*x_tl), x_tl_err_up /(np.log(10)*x_tl)], fmt='none', ecolor= "r")
+    plt.errorbar(nii_ha, oiii_hb, yerr = oiii_hb_err, xerr = nii_ha_err, fmt='none', ecolor= "r")
     plt.xlabel("log([NII]6584/Halpha)")
     plt.ylabel("log([OIII]5007/Hbeta)")
+    plt.text(-2, 0, "Starbust")
+    plt.text(-0.4, -0.2, "Composite")
+    plt.text(0.5, 0.8, "AGN")
     plt.xlim(-3, 1)
     plt.ylim(-1, 1)
     plt.title("BPT diagram for [NII]")
@@ -93,13 +104,15 @@ def extract_and_plot():
     plt.savefig("BPT_NII.png")
     plt.show()
 
-
-    plt.scatter(x_tr, y, s = 4, c = "g")
+    #SII BPT diagram
     plt.plot(x_kewley, y_kewleys2, linestyle = "-", c = "k", label = "Kewley 2001")
-    #plt.errorbar(x_tr, y, yerr = [y_err_down / (np.log(10) * y), y_err_up  / (np.log(10) * y)], xerr = [x_tr_err_down / (np.log(10)*x_tr), x_tr_err_up /(np.log(10)*x_tr)], fmt='none', ecolor= "g")
-    
+    plt.errorbar(sii_ha, oiii_hb, yerr = oiii_hb_err, xerr = sii_ha_err, fmt='none', ecolor= "g")
     plt.xlabel("log([SII]6717,31/Halpha)")
     plt.ylabel("log([OIII]5007/Hbeta)")
+    plt.text(-2.5, -0.5, "Starburst")
+    plt.text(0.3, -0.5, "LI(N)ER")
+    plt.text(-0.2, 0.9, "Seyfert")
+    plt.text
     plt.xlim(-3, 1)
     plt.ylim(-1, 1)
     plt.title("BPT diagram for [SII]")
@@ -107,20 +120,20 @@ def extract_and_plot():
     plt.savefig("BPT_SII.png")
     plt.show()
 
-    plt.scatter(x_b, y, s = 4, c = "b")
+    #OI BPT diagram
     plt.plot(x_kewley, y_kewleyo1, linestyle = "-", c = "k", label = "Kewley 2001")
-    #plt.errorbar(x_b, y, yerr = [y_err_down / (np.log(10) * y), y_err_up  / (np.log(10) * y)], xerr = [x_b_err_down / (np.log(10)*x_b), x_b_err_up /(np.log(10)*x_b)], fmt='none', ecolor= "b")
-    plt.xlabel("log([NII]6584/Halpha)")
+    plt.errorbar(oi_ha, oiii_hb, yerr = oiii_hb_err, xerr = oi_ha_err, fmt='none', ecolor= "b")
+    plt.xlabel("log([OI]6300}/Halpha)")
     plt.ylabel("log([OIII]5007/Hbeta)")
+    plt.text(-2.5, -0.5, "Starburst")
+    plt.text(-1, -0.1, "LI(N)ER")
+    plt.text(-1.5, 0.74, "Seyfert")
     plt.xlim(-3, -0.7)
     plt.ylim(-1, 1)
     plt.title("BPT diagram for [OI]")
     plt.legend()
     plt.savefig("BPT_OI.png")
     plt.show()
-    
-
-    
 
 def main():
     extract_and_plot()
