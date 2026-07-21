c189e365f403708bf53e8a2a0ef69451c2d4ffcc
David-Clayton <s2413476@ed.ac.uk>
2026-07-21 21:11:54 +0100

    Replaced strong-line error calculations with a Monte Carlo

--- Strong-line Abundances/StrongLinesMainandPlot.py
+++ Strong-line Abundances/StrongLinesMainandPlot.py
@@ -35,30 +35,59 @@ def CLASSY():
 
     h_alpha = pd.read_excel(classy_cel_filepath, usecols = "BL", skiprows = 0).to_numpy().flatten()[0:45]
     h_beta = pd.read_excel(classy_cel_filepath, usecols = "AN", skiprows = 0).to_numpy().flatten()[0:45]
-    h_gamma = pd.read_excel(classy_cel_filepath, usecols = "N", skiprows = 0).to_numpy().flatten()[0:45]
 
     o_ii_3727 = pd.read_excel(classy_cel_filepath, usecols = "B", skiprows = 0).to_numpy().flatten()[0:45]
-    o_ii_3729 = pd.read_excel(classy_cel_filepath, usecols = "F", skiprows = 0).to_numpy().flatten()[0:45]
-    o_ii_7320 = pd.read_excel(classy_cel_filepath, usecols = "CB", skiprows = 0).to_numpy().flatten()[0:45]
-    o_ii_7330 = pd.read_excel(classy_cel_filepath, usecols = "CF", skiprows = 0).to_numpy().flatten()[0:45]
-
+    
     o_iii_5007 = pd.read_excel(classy_cel_filepath, usecols = "AV", skiprows = 0).to_numpy().flatten()[0:45]
-    o_iii_4959 = pd.read_excel(classy_cel_filepath, usecols = "AR", skiprows = 0).to_numpy().flatten()[0:45]
-    o_iii_4363 = pd.read_excel(classy_cel_filepath, usecols = "R", skiprows = 0).to_numpy().flatten()[0:45]
 
     n_ii_6584 = pd.read_excel(classy_cel_filepath, usecols = "BP", skiprows = 0).to_numpy().flatten()[0:45]
 
-    s_ii_6717 = pd.read_excel(classy_cel_filepath, usecols = "BT", skiprows = 0).to_numpy().flatten()[0:45]
-    s_ii_6731 = pd.read_excel(classy_cel_filepath, usecols = "BX", skiprows = 0).to_numpy().flatten()[0:45]
-
-    s_iii_6312 = pd.read_excel(classy_cel_filepath, usecols = "BH", skiprows = 0).to_numpy().flatten()[0:45]
-    s_iii_9069 = pd.read_excel(classy_cel_filepath, usecols = "CN", skiprows = 0).to_numpy().flatten()[0:45]
-
-    ar_iii_7135 = pd.read_excel(classy_cel_filepath, usecols = "CJ", skiprows = 0).to_numpy().flatten()[0:45]
-
     ne_iii_3869 = pd.read_excel(classy_cel_filepath, usecols = "CR", skiprows = 0).to_numpy().flatten()[0:45]
 
-    fe_iii_4658 = pd.read_excel(classy_cel_filepath, usecols = "Z", skiprows = 0).to_numpy().flatten()[0:45]
+
+    h_alpha_err_u = pd.read_excel(classy_cel_filepath, usecols = "BM", skiprows = 0).to_numpy().flatten()[0:45]
+    h_beta_err_u = pd.read_excel(classy_cel_filepath, usecols = "AO", skiprows = 0).to_numpy().flatten()[0:45]
+
+    o_ii_3727_err_u = pd.read_excel(classy_cel_filepath, usecols = "C", skiprows = 0).to_numpy().flatten()[0:45]
+    
+    o_iii_5007_err_u = pd.read_excel(classy_cel_filepath, usecols = "AW", skiprows = 0).to_numpy().flatten()[0:45]
+
+    n_ii_6584_err_u = pd.read_excel(classy_cel_filepath, usecols = "BQ", skiprows = 0).to_numpy().flatten()[0:45]
+
+    ne_iii_3869_err_u = pd.read_excel(classy_cel_filepath, usecols = "CS", skiprows = 0).to_numpy().flatten()[0:45]
+
+
+    h_alpha_err_d = pd.read_excel(classy_cel_filepath, usecols = "BN", skiprows = 0).to_numpy().flatten()[0:45]
+    h_beta_err_d = pd.read_excel(classy_cel_filepath, usecols = "AP", skiprows = 0).to_numpy().flatten()[0:45]
+
+    o_ii_3727_err_d = pd.read_excel(classy_cel_filepath, usecols = "D", skiprows = 0).to_numpy().flatten()[0:45]
+    
+    o_iii_5007_err_d = pd.read_excel(classy_cel_filepath, usecols = "AX", skiprows = 0).to_numpy().flatten()[0:45]
+
+    n_ii_6584_err_d = pd.read_excel(classy_cel_filepath, usecols = "BR", skiprows = 0).to_numpy().flatten()[0:45]
+
+    ne_iii_3869_err_d = pd.read_excel(classy_cel_filepath, usecols = "CT", skiprows = 0).to_numpy().flatten()[0:45]
+
+
+    h_alpha_err = (h_alpha_err_u + h_alpha_err_d) / 2
+    h_beta_err = (h_beta_err_u + h_beta_err_d) / 2
+
+    o_ii_3727_err = (o_ii_3727_err_u + o_ii_3727_err_d) / 2
+    
+    o_iii_5007_err = (o_iii_5007_err_u + o_iii_5007_err_d) / 2
+    n_ii_6584_err = (n_ii_6584_err_u + n_ii_6584_err_d) / 2
+
+    ne_iii_3869_err = (ne_iii_3869_err_u + ne_iii_3869_err_d) / 2
+
+    #Extend strong-line intensities to an array of intensities with the mean at the empirical value
+    #And the standard deviation as the error on the empirical value
+
+    h_alpha = np.random.normal(h_alpha[:, None], h_alpha_err[:, None], size = (45, 1000))
+    h_beta = np.random.normal(h_beta[:, None], h_beta_err[:, None], size = (45, 1000))
+    o_ii_3727 = np.random.normal(o_ii_3727[:, None], o_ii_3727_err[:, None], size = (45, 1000))
+    o_iii_5007 = np.random.normal(o_iii_5007[:, None], o_iii_5007_err[:, None], size = (45, 1000))
+    n_ii_6584 = np.random.normal(n_ii_6584[:, None], n_ii_6584_err[:, None], size = (45, 1000))
+    ne_iii_3869 = np.random.normal(ne_iii_3869[:, None], ne_iii_3869_err[:, None], size = (45, 1000))
 
     #Direct method metallicity
     direct_Z = pd.read_csv(classy_data_filepath, delimiter = ",", usecols = [6], header = 0).to_numpy().flatten()
@@ -69,31 +98,41 @@ def CLASSY():
     #Strong line metallicities
     #R2 method
     R2 = np.log10(o_ii_3727 / h_beta)
-    Z_R2 = metallicity(8.00, 0.92, 0.40, 0.17, R2) #For t^2 > 0 (inhomogeneous case)
-    sigma_R2 = error(-0.20, -0.05, 0.36, R2)
+    Z_R2_dist = metallicity(8.00, 0.92, 0.40, 0.17, R2)  # For t^2 > 0 (inhomogeneous case)
+    Z_R2_median = np.nanmedian(Z_R2_dist, axis = 1)
+    Z_R2_error_up = np.nanpercentile(Z_R2_dist, 84, axis = 1) - Z_R2_median
+    Z_R2_error_down = Z_R2_median - np.nanpercentile(Z_R2_dist, 16, axis = 1)
 
     #O32 method
     O32 = np.log10(o_iii_5007 / o_ii_3727)
-    Z_O32 = metallicity(8.33, -0.32, 0.08, -0.09, O32)
-    sigma_O32 = error(-0.02, 0.02, 0.30, O32)
+    Z_O32_dist = metallicity(8.33, -0.32, 0.08, -0.09, O32)
+    Z_O32_median = np.nanmedian(Z_O32_dist, axis = 1)
+    Z_O32_error_up = np.nanpercentile(Z_O32_dist, 84, axis = 1) - Z_O32_median
+    Z_O32_error_down = Z_O32_median - np.nanpercentile(Z_O32_dist, 16, axis = 1)
 
     #Ne3O2 method
     Ne3O2 = np.log10(ne_iii_3869 / o_ii_3727)
-    Z_Ne3O2 = metallicity(7.85, -0.71, -0.26, -0.05, Ne3O2)
-    sigma_Ne3O2 = error(-0.02, -0.04, 0.26, Ne3O2)
+    Z_Ne3O2_dist = metallicity(7.85, -0.71, -0.26, -0.05, Ne3O2)
+    Z_Ne3O2_median = np.nanmedian(Z_Ne3O2_dist, axis = 1)
+    Z_Ne3O2_error_up = np.nanpercentile(Z_Ne3O2_dist, 84, axis = 1) - Z_Ne3O2_median
+    Z_Ne3O2_error_down = Z_Ne3O2_median - np.nanpercentile(Z_Ne3O2_dist, 16, axis = 1)
 
     #R2Ne3 method
     R2Ne3 = np.log10((o_ii_3727 + ne_iii_3869) / h_beta)
-    Z_R2Ne3 = metallicity(7.83, 1.43, 0.00, 0.00, R2Ne3)
-    sigma_R2Ne3 = error(-0.57, 0.15, 0.31, R2Ne3)
+    Z_R2Ne3_dist = metallicity(7.83, 1.43, 0.00, 0.00, R2Ne3)
+    Z_R2Ne3_median = np.nanmedian(Z_R2Ne3_dist, axis = 1)
+    Z_R2Ne3_error_up = np.nanpercentile(Z_R2Ne3_dist, 84, axis = 1) - Z_R2Ne3_median
+    Z_R2Ne3_error_down = Z_R2Ne3_median - np.nanpercentile(Z_R2Ne3_dist, 16, axis = 1)
 
     #N2 method
     N2 = np.log10(n_ii_6584 / h_alpha)
-    Z_N2 = metallicity(9.27, 1.48, 0.86, 0.23, N2)
-    sigma_N2 = error(0.06, 0.17, 0.33, N2)
+    Z_N2_dist = metallicity(9.27, 1.48, 0.86, 0.23, N2)
+    Z_N2_median = np.nanmedian(Z_N2_dist, axis = 1)
+    Z_N2_error_up = np.nanpercentile(Z_N2_dist, 84, axis = 1) - Z_N2_median
+    Z_N2_error_down = Z_N2_median - np.nanpercentile(Z_N2_dist, 16, axis = 1)
 
-    return (direct_Z, direct_Z_err, Z_R2, sigma_R2, Z_O32, sigma_O32, Z_Ne3O2, 
-            sigma_Ne3O2, Z_R2Ne3, sigma_R2Ne3, Z_N2, sigma_N2, names)
+    return (direct_Z, direct_Z_err, Z_R2_median, (Z_R2_error_up + Z_R2_error_down)/2, Z_O32_median, (Z_O32_error_up + Z_O32_error_down)/2, Z_Ne3O2_median, (Z_Ne3O2_error_up + Z_Ne3O2_error_down)/2,
+    Z_R2Ne3_median, (Z_R2Ne3_error_up + Z_R2Ne3_error_down)/2, Z_N2_median, (Z_N2_error_up + Z_N2_error_down)/2, names)
 
 
 def LzLCS():
@@ -106,34 +145,49 @@ def LzLCS():
 
     h_alpha = pd.read_excel(lzlcs_cel_filepath, usecols = "AN", skiprows = 0).to_numpy().flatten()[0:27]
     h_beta = pd.read_excel(lzlcs_cel_filepath, usecols = "Z", skiprows = 0).to_numpy().flatten()[0:27]
-    h_gamma = pd.read_excel(lzlcs_cel_filepath, usecols = "T", skiprows = 0).to_numpy().flatten()[0:27]
-
+   
     o_ii_3727 = pd.read_excel(lzlcs_cel_filepath, usecols = "J", skiprows = 0).to_numpy().flatten()[0:27]
-    o_ii_3729 = pd.read_excel(lzlcs_cel_filepath, usecols = "L", skiprows = 0).to_numpy().flatten()[0:27]
 
     o_iii_5007 = pd.read_excel(lzlcs_cel_filepath, usecols = "AD", skiprows = 0).to_numpy().flatten()[0:27]
-    o_iii_4959 = pd.read_excel(lzlcs_cel_filepath, usecols = "AB", skiprows = 0).to_numpy().flatten()[0:27]
-    o_iii_4363 = pd.read_excel(lzlcs_cel_filepath, usecols = "V", skiprows = 0).to_numpy().flatten()[0:27]
-
-    n_ii_6584 = pd.read_excel(lzlcs_cel_filepath, usecols = "AP", skiprows = 0).to_numpy().flatten()[0:45]
-
-    s_ii_6717 = pd.read_excel(lzlcs_cel_filepath, usecols = "AR", skiprows = 0).to_numpy().flatten()[0:27]
-    s_ii_6731 = pd.read_excel(lzlcs_cel_filepath, usecols = "AT", skiprows = 0).to_numpy().flatten()[0:27]
+   
+    n_ii_6584 = pd.read_excel(lzlcs_cel_filepath, usecols = "AP", skiprows = 0).to_numpy().flatten()[0:27]
 
     ne_iii_3869 = pd.read_excel(lzlcs_cel_filepath, usecols = "N", skiprows = 0).to_numpy().flatten()[0:27]
 
+    h_alpha_err = pd.read_excel(lzlcs_cel_filepath, usecols="AO", skiprows=0).to_numpy().flatten()[0:27]
+    h_beta_err = pd.read_excel(lzlcs_cel_filepath, usecols="AA", skiprows=0).to_numpy().flatten()[0:27]
+    o_ii_3727_err = pd.read_excel(lzlcs_cel_filepath, usecols="K", skiprows=0).to_numpy().flatten()[0:27]
+    o_iii_5007_err = pd.read_excel(lzlcs_cel_filepath, usecols="AE", skiprows=0).to_numpy().flatten()[0:27]
+    n_ii_6584_err = pd.read_excel(lzlcs_cel_filepath, usecols="AQ", skiprows=0).to_numpy().flatten()[0:27]
+    ne_iii_3869_err = pd.read_excel(lzlcs_cel_filepath, usecols="O", skiprows=0).to_numpy().flatten()[0:27]
+
     h_alpha = fix_nan_issues(h_alpha)
+    h_alpha_err = fix_nan_issues(h_alpha_err)
+
     h_beta = fix_nan_issues(h_beta)
-    h_gamma = fix_nan_issues(h_gamma)
+    h_beta_err = fix_nan_issues(h_beta_err)
+
     o_ii_3727 = fix_nan_issues(o_ii_3727)
-    o_ii_3729 = fix_nan_issues(o_ii_3729)
+    o_ii_3727_err = fix_nan_issues(o_ii_3727_err)
+
     o_iii_5007 = fix_nan_issues(o_iii_5007)
-    o_iii_4959 = fix_nan_issues(o_iii_4959)
-    o_iii_4363 = fix_nan_issues(o_iii_4363)
+    o_iii_5007_err = fix_nan_issues(o_iii_5007_err)
+
     n_ii_6584 = fix_nan_issues(n_ii_6584)
-    s_ii_6717 = fix_nan_issues(s_ii_6717)
-    s_ii_6731 = fix_nan_issues(s_ii_6731)
+    n_ii_6584_err = fix_nan_issues(n_ii_6584_err)
+
     ne_iii_3869 = fix_nan_issues(ne_iii_3869)
+    ne_iii_3869_err = fix_nan_issues(ne_iii_3869_err)
+
+    #Extend strong-line intensities to an array of intensities with the mean at the empirical value
+    #And the standard deviation as the error on the empirical value
+
+    h_alpha = np.random.normal(h_alpha[:, None], h_alpha_err[:, None], size = (27, 1000))
+    h_beta = np.random.normal(h_beta[:, None], h_beta_err[:, None], size = (27, 1000))
+    o_ii_3727 = np.random.normal(o_ii_3727[:, None], o_ii_3727_err[:, None], size = (27, 1000))
+    o_iii_5007 = np.random.normal(o_iii_5007[:, None], o_iii_5007_err[:, None], size = (27, 1000))
+    n_ii_6584 = np.random.normal(n_ii_6584[:, None], n_ii_6584_err[:, None], size = (27, 1000))
+    ne_iii_3869 = np.random.normal(ne_iii_3869[:, None], ne_iii_3869_err[:, None], size = (27, 1000))
 
     #Direct method metallicity
     direct_Z = pd.read_csv(lzlcs_data_filepath, delimiter = ",", usecols = [8], header = 0).to_numpy().flatten()
@@ -141,35 +195,46 @@ def LzLCS():
     direct_Z_err_down = pd.read_csv(lzlcs_data_filepath, delimiter = ",", usecols = [10], header = 0).to_numpy().flatten()
     direct_Z_err = (direct_Z_err_up + direct_Z_err_down) / 2
 
-     #Strong line metallicities
+    #Strong line metallicities
     #R2 method
     R2 = np.log10(o_ii_3727 / h_beta)
-    Z_R2 = metallicity(8.00, 0.92, 0.40, 0.17, R2) #For t^2 > 0 (inhomogeneous case)
-    sigma_R2 = error(-0.20, -0.05, 0.36, R2)
+    Z_R2_dist = metallicity(8.00, 0.92, 0.40, 0.17, R2)
+    Z_R2_median = np.nanmedian(Z_R2_dist, axis = 1)
+    Z_R2_error_up = np.nanpercentile(Z_R2_dist, 84, axis = 1) - Z_R2_median
+    Z_R2_error_down = Z_R2_median - np.nanpercentile(Z_R2_dist, 16, axis = 1)
 
-    #O32 method
+    # O32 method
     O32 = np.log10(o_iii_5007 / o_ii_3727)
-    Z_O32 = metallicity(8.33, -0.32, 0.08, -0.09, O32)
-    sigma_O32 = error(-0.02, 0.02, 0.30, O32)
+    Z_O32_dist = metallicity(8.33, -0.32, 0.08, -0.09, O32)
+    Z_O32_median = np.nanmedian(Z_O32_dist, axis = 1)
+    Z_O32_error_up = np.nanpercentile(Z_O32_dist, 84, axis = 1) - Z_O32_median
+    Z_O32_error_down = Z_O32_median - np.nanpercentile(Z_O32_dist, 16, axis = 1)
 
     #Ne3O2 method
     Ne3O2 = np.log10(ne_iii_3869 / o_ii_3727)
-    Z_Ne3O2 = metallicity(7.85, -0.71, -0.26, -0.05, Ne3O2)
-    sigma_Ne3O2 = error(-0.02, -0.04, 0.26, Ne3O2)
+    Z_Ne3O2_dist = metallicity(7.85, -0.71, -0.26, -0.05, Ne3O2)
+    Z_Ne3O2_median = np.nanmedian(Z_Ne3O2_dist, axis = 1)
+    Z_Ne3O2_error_up = np.nanpercentile(Z_Ne3O2_dist, 84, axis = 1) - Z_Ne3O2_median
+    Z_Ne3O2_error_down = Z_Ne3O2_median - np.nanpercentile(Z_Ne3O2_dist, 16, axis = 1)
 
-    #R2Ne3 method
+    # R2Ne3 method
     R2Ne3 = np.log10((o_ii_3727 + ne_iii_3869) / h_beta)
-    Z_R2Ne3 = metallicity(7.83, 1.43, 0.00, 0.00, R2Ne3)
-    sigma_R2Ne3 = error(-0.57, 0.15, 0.31, R2Ne3)
+    Z_R2Ne3_dist = metallicity(7.83, 1.43, 0.00, 0.00, R2Ne3)
+    Z_R2Ne3_median = np.nanmedian(Z_R2Ne3_dist, axis = 1)
+    Z_R2Ne3_error_up = np.nanpercentile(Z_R2Ne3_dist, 84, axis = 1) - Z_R2Ne3_median
+    Z_R2Ne3_error_down = Z_R2Ne3_median - np.nanpercentile(Z_R2Ne3_dist, 16, axis = 1)
 
     #N2 method
     N2 = np.log10(n_ii_6584 / h_alpha)
-    Z_N2 = metallicity(9.27, 1.48, 0.86, 0.23, N2)
-    sigma_N2 = error(0.06, 0.17, 0.33, N2)
+    Z_N2_dist = metallicity(9.27, 1.48, 0.86, 0.23, N2)
+    Z_N2_median = np.nanmedian(Z_N2_dist, axis = 1)
+    Z_N2_error_up = np.nanpercentile(Z_N2_dist, 84, axis = 1) - Z_N2_median
+    Z_N2_error_down = Z_N2_median - np.nanpercentile(Z_N2_dist, 16, axis = 1)
 
-    return (direct_Z, direct_Z_err, Z_R2, sigma_R2, Z_O32, sigma_O32, Z_Ne3O2, 
-            sigma_Ne3O2, Z_R2Ne3, sigma_R2Ne3, Z_N2, sigma_N2, names)
-
+    return (
+        direct_Z, direct_Z_err, Z_R2_median, (Z_R2_error_up + Z_R2_error_down) / 2, Z_O32_median, (Z_O32_error_up + Z_O32_error_down) / 2,
+        Z_Ne3O2_median, (Z_Ne3O2_error_up + Z_Ne3O2_error_down) / 2, Z_R2Ne3_median, (Z_R2Ne3_error_up + Z_R2Ne3_error_down) / 2, Z_N2_median, (Z_N2_error_up + Z_N2_error_down) / 2,
+        names)
 
 def main():
     (Z_dir_C, Z_dir_err_C, Z_R2_C, Z_R2_err_C, Z_O32_C, Z_O32_err_C, Z_Ne3O2_C, 
